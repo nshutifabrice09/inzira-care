51f9acc2bb2df7418a46ef8557fe1a37ae487e9c
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-02 13:45:27 +0200

    initial commit

---
+++ src/test/java/com/backend/inzira_care/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.inzira_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
