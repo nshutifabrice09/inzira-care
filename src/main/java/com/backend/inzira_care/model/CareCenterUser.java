bf0329db99aae5247d379067dbbcba66fbb9f905
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-05 12:48:19 +0200

    final-entities-updates

--- src/main/java/com/backend/inzira_care/model/CareCenterUser.java
+++ src/main/java/com/backend/inzira_care/model/CareCenterUser.java
@@ -1,6 +1,19 @@
 package com.backend.inzira_care.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "care_center_users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CareCenterUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private Long userId;
     private Long careCenterId;
--- src/main/java/com/backend/inzira_care/model/DailyLog.java
+++ src/main/java/com/backend/inzira_care/model/DailyLog.java
@@ -1,8 +1,21 @@
 package com.backend.inzira_care.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "daily_logs")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DailyLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private Long elderlyPersonId;
     private String summary;
