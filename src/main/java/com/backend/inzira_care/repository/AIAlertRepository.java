b918e17c1490a0d14d2b48823158d625d471f878
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 11:31:03 +0200

    third-repo-update

--- src/main/java/com/backend/inzira_care/repository/AIAlertRepository.java
+++ src/main/java/com/backend/inzira_care/repository/AIAlertRepository.java
@@ -2,6 +2,8 @@ package com.backend.inzira_care.repository;
 
 import com.backend.inzira_care.model.AIAlert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AIAlertRepository extends JpaRepository <AIAlert, Long> {
 }
--- src/main/java/com/backend/inzira_care/repository/HealthDeviceRepository.java
+++ src/main/java/com/backend/inzira_care/repository/HealthDeviceRepository.java
@@ -2,6 +2,8 @@ package com.backend.inzira_care.repository;
 
 import com.backend.inzira_care.model.HealthDevice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HealthDeviceRepository extends JpaRepository <HealthDevice, Long> {
 }
