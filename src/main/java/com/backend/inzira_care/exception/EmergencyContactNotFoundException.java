1cbcc0316cd0929bd5763813e39c417aa47ef3cf
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 12:27:36 +0200

    fith-exception-commit

--- src/main/java/com/backend/inzira_care/exception/EmergencyContactNotFoundException.java
+++ src/main/java/com/backend/inzira_care/exception/EmergencyContactNotFoundException.java
@@ -1,2 +1,7 @@
-package com.backend.inzira_care.exception;public class EmergencyContactNotFoundException {
+package com.backend.inzira_care.exception;
+
+public class EmergencyContactNotFoundException extends RuntimeException{
+    public EmergencyContactNotFoundException(Long id){
+        super("Couldn't find an EmergencyContact with id "+id);
+    }
 }
--- src/main/java/com/backend/inzira_care/exception/HealthDeviceNotFoundException.java
+++ src/main/java/com/backend/inzira_care/exception/HealthDeviceNotFoundException.java
@@ -1,2 +1,7 @@
-package com.backend.inzira_care.exception;public class HealthDeviceNotFoundException {
+package com.backend.inzira_care.exception;
+
+public class HealthDeviceNotFoundException extends RuntimeException{
+    public HealthDeviceNotFoundException(Long id){
+        super("Couldn't find a HealthDevice with id "+id);
+    }
 }
