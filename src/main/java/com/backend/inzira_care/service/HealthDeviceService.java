74d29c4a804891ff59633d409e0fcc9d2b90d749
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 10:38:53 +0200

    initial-serviceImpl-commit

--- src/main/java/com/backend/inzira_care/service/HealthDeviceService.java
+++ src/main/java/com/backend/inzira_care/service/HealthDeviceService.java
@@ -1,2 +1,13 @@
-package com.backend.inzira_care.service;public interface HealthDeviceService {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.HealthDevice;
+
+import java.util.List;
+
+public interface HealthDeviceService {
+    List<HealthDevice> getAllHealthDevices();
+    HealthDevice getHealthDeviceById(Long id);
+    HealthDevice saveHealthDevice(HealthDevice healthDevice);
+    HealthDevice updateHealthDevice(Long id, HealthDevice healthDevice);
+    void removeHealthDeviceById(Long id);
 }
--- src/main/java/com/backend/inzira_care/service/UserService.java
+++ src/main/java/com/backend/inzira_care/service/UserService.java
@@ -1,2 +1,13 @@
-package com.backend.inzira_care.service;public interface UserService {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User saveUser(User user);
+    User updateUser(Long id, User user);
+    void removeUserById (Long id);
 }
