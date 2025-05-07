a5382cbd6376101b53f6e20abe89d473eb432056
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 12:00:21 +0200

    third-exception-commit

--- src/main/java/com/backend/inzira_care/exception/AuditLogNotFoundException.java
+++ src/main/java/com/backend/inzira_care/exception/AuditLogNotFoundException.java
@@ -1,2 +1,9 @@
-package com.backend.inzira_care.exception;public class AuditLogNotFoundException {
+package com.backend.inzira_care.exception;
+
+import com.backend.inzira_care.model.AuditLog;
+
+public class AuditLogNotFoundException extends RuntimeException{
+    public AuditLogNotFoundException(Long id){
+        super ("Couldn't find an AuditLog with id "+id);
+    }
 }
--- src/main/java/com/backend/inzira_care/exception/CareCenterNotFoundException.java
+++ src/main/java/com/backend/inzira_care/exception/CareCenterNotFoundException.java
@@ -1,7 +1,7 @@
 package com.backend.inzira_care.exception;
 
-public class CareCenterNotFoundExcpetion extends RuntimeException{
-    public CareCenterNotFoundExcpetion(Long id){
+public class CareCenterNotFoundException extends RuntimeException{
+    public CareCenterNotFoundException(Long id){
         super("Couldn't find a CareCenter with id "+id);
     }
 }
