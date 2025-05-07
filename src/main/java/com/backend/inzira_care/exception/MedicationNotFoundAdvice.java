5bfc582f2d050a6220b198431df95e2a71fcc432
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 22:18:09 +0200

    final-exception-advice

--- src/main/java/com/backend/inzira_care/exception/MedicationNotFoundAdvice.java
+++ src/main/java/com/backend/inzira_care/exception/MedicationNotFoundAdvice.java
@@ -1,2 +1,22 @@
-package com.backend.inzira_care.exception;public class MedicationNotFoundAdvice {
+package com.backend.inzira_care.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class MedicationNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(MedicationLogNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> exceptionHandler(MedicalScheduleNotFoundException exception){
+        Map<String,String> errorMap=new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+        return errorMap;
+    }
 }
