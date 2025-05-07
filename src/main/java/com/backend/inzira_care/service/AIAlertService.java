e1767bf90c88393147af13c61cdfbc993b14c136
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 10:14:19 +0200

    second-service-commit

--- src/main/java/com/backend/inzira_care/service/AIAlertService.java
+++ src/main/java/com/backend/inzira_care/service/AIAlertService.java
@@ -1,2 +1,13 @@
-package com.backend.inzira_care.service;public interface AIAlertService {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.AIAlert;
+
+import java.util.List;
+
+public interface AIAlertService {
+    List<AIAlert> getAllAiAlerts();
+    AIAlert getAiAlertById(Long id);
+    AIAlert saveAiAlert(AIAlert aiAlert);
+    AIAlert updateAiAlert(Long id, AIAlert aiAlert);
+    void deleteById(Long id);
 }
--- src/main/java/com/backend/inzira_care/service/AppointmentRepository.java
+++ src/main/java/com/backend/inzira_care/service/AppointmentRepository.java
@@ -1,2 +1,13 @@
-package com.backend.inzira_care.service;public interface AppointmentRepository {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.Appointment;
+
+import java.util.List;
+
+public interface AppointmentRepository {
+    List<Appointment> getAllAppointments();
+    Appointment getAppointmentById(Long id);
+    Appointment saveAppointment(Appointment appointment);
+    Appointment updateAppointment(Long id, Appointment appointment);
+    void deleteAppointmentById(Long id);
 }
