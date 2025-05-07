f0ff2a04d84eb191a7779c6f8d264fba33fc15ff
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 12:59:52 +0200

    latest-exception-commit

--- src/main/java/com/backend/inzira_care/exception/MessageNotFoundException.java
+++ src/main/java/com/backend/inzira_care/exception/MessageNotFoundException.java
@@ -1,2 +1,8 @@
-package com.backend.inzira_care.exception;public class MessageNotFoundException {
+package com.backend.inzira_care.exception;
+
+public class MessageNotFoundException extends RuntimeException{
+    public MessageNotFoundException(Long id){
+        super
+                ("Couldn't find a Message with id "+id);
+    }
 }
--- src/main/java/com/backend/inzira_care/exception/NotificationNotFoundException.java
+++ src/main/java/com/backend/inzira_care/exception/NotificationNotFoundException.java
@@ -1,2 +1,8 @@
-package com.backend.inzira_care.exception;public class NotificationNotFoundException {
+package com.backend.inzira_care.exception;
+
+public class NotificationNotFoundException extends RuntimeException{
+    public NotificationNotFoundException(Long id){
+        super
+                ("Couldn't find a Notification with id "+id);
+    }
 }
