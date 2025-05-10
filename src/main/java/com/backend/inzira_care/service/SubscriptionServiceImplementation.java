3c4fd92cce039155b96ef65bca2747a9d907d774
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:49:56 +0200

    service-updates

--- src/main/java/com/backend/inzira_care/service/SubscriptionServiceImplementation.java
+++ src/main/java/com/backend/inzira_care/service/SubscriptionServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.backend.inzira_care.service;
 
+import com.backend.inzira_care.model.Subscription;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubscriptionServiceImplementation implements SubscriptionService{
+    @Override
+    public List<Subscription> getAllSubscriptions() {
+        return null;
+    }
+
+    @Override
+    public Subscription getSubscriptionById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Subscription saveSubscription(Subscription subscription) {
+        return null;
+    }
+
+    @Override
+    public Subscription updateSubscription(Long id, Subscription subscription) {
+        return null;
+    }
+
+    @Override
+    public void removeSubscriptionById(Long id) {
+
+    }
 }
