090b82f751605ab83f0e998635ee584224a133b8
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:50:39 +0200

    service-updates

--- src/main/java/com/backend/inzira_care/service/UserElderlyLinkService.java
+++ src/main/java/com/backend/inzira_care/service/UserElderlyLinkService.java
@@ -1,2 +1,9 @@
-package com.backend.inzira_care.service;public interface UserElderlyService {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.UserElderlyLink;
+
+import java.util.List;
+
+public interface UserElderlyLinkService {
+    List<UserElderlyLink>
 }
