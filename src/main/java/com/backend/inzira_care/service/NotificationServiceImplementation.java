97b2248ae2ed2652b42605e12218f8476d329bdc
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:48:56 +0200

    service-updates

--- src/main/java/com/backend/inzira_care/service/NotificationServiceImplementation.java
+++ src/main/java/com/backend/inzira_care/service/NotificationServiceImplementation.java
@@ -1,2 +1,35 @@
-package com.backend.inzira_care.service;public class NotificationServiceImplementation {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.Notification;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationServiceImplementation implements NotificationService{
+
+    @Override
+    public List<Notification> getAllNotifications() {
+        return null;
+    }
+
+    @Override
+    public Notification getNotificationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Notification saveNotification(Notification notification) {
+        return null;
+    }
+
+    @Override
+    public Notification updateNotification(Long id, Notification notification) {
+        return null;
+    }
+
+    @Override
+    public void removeNotificationById(Long id) {
+
+    }
 }
