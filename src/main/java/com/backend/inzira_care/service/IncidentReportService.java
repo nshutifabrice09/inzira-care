d1210a298e7130e499d4707ee08375f710e9e41d
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:29:17 +0200

    service-impl-updates

--- src/main/java/com/backend/inzira_care/service/IncidentReportService.java
+++ src/main/java/com/backend/inzira_care/service/IncidentReportService.java
@@ -1,2 +1,13 @@
-package com.backend.inzira_care.service;public interface IncidentReportService {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.IncidentReport;
+
+import java.util.List;
+
+public interface IncidentReportService {
+    List<IncidentReport> getAllIncidentReports();
+    IncidentReport getIncidentReportById(Long id);
+    IncidentReport saveIncidentReport(IncidentReport incidentReport);
+    IncidentReport updateIncidentReport(Long id, IncidentReport incidentReport);
+    void removeIncidentReportById(Long id);
 }
