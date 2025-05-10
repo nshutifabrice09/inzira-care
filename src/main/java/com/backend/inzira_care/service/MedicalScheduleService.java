18ea3bc0a2b623d8822e862d4c061151db21c409
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:34:59 +0200

    service-impl-updates

--- src/main/java/com/backend/inzira_care/service/MedicalScheduleService.java
+++ src/main/java/com/backend/inzira_care/service/MedicalScheduleService.java
@@ -1,2 +1,13 @@
-package com.backend.inzira_care.service;public interface MedicalScheduleService {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.MedicalSchedule;
+
+import java.util.List;
+
+public interface MedicalScheduleService {
+    List<MedicalSchedule> getAllMedicalSchedules();
+    MedicalSchedule getMedicalScheduleById(Long id);
+    MedicalSchedule saveMedicalSchedule(MedicalSchedule medicalSchedule);
+    MedicalSchedule updateMedicalSchedule(Long id, MedicalSchedule medicalSchedule);
+    void removeMedicalScheduleById(Long id);
 }
