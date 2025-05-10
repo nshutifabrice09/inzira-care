5e97516415ae70d8b34c93a1baa2bf8ed0515ddd
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:37:50 +0200

    service-updates

--- src/main/java/com/backend/inzira_care/service/MedicalScheduleServiceImplementation.java
+++ src/main/java/com/backend/inzira_care/service/MedicalScheduleServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.inzira_care.service;public class MedicalScheduleServiceImplementation {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.MedicalSchedule;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MedicalScheduleServiceImplementation implements MedicalScheduleService{
+    @Override
+    public List<MedicalSchedule> getAllMedicalSchedules() {
+        return null;
+    }
+
+    @Override
+    public MedicalSchedule getMedicalScheduleById(Long id) {
+        return null;
+    }
+
+    @Override
+    public MedicalSchedule saveMedicalSchedule(MedicalSchedule medicalSchedule) {
+        return null;
+    }
+
+    @Override
+    public MedicalSchedule updateMedicalSchedule(Long id, MedicalSchedule medicalSchedule) {
+        return null;
+    }
+
+    @Override
+    public void removeMedicalScheduleById(Long id) {
+
+    }
 }
