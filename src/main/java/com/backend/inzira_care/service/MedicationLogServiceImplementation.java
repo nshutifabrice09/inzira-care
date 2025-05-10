4a7d17dd9078a2703df79f5c59991c1467258c73
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:43:10 +0200

    service-updates

--- src/main/java/com/backend/inzira_care/service/MedicationLogServiceImplementation.java
+++ src/main/java/com/backend/inzira_care/service/MedicationLogServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.inzira_care.service;public class MedicalLogServiceImplementation {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.MedicationLog;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MedicationLogServiceImplementation implements MedicationLogService{
+    @Override
+    public List<MedicationLog> getAllMedicationLogs() {
+        return null;
+    }
+
+    @Override
+    public MedicationLog getMedicationLogById(Long id) {
+        return null;
+    }
+
+    @Override
+    public MedicationLog saveMedicationLog(MedicationLog medicationLog) {
+        return null;
+    }
+
+    @Override
+    public MedicationLog updateMedicationLog(Long id, MedicationLog medicationLog) {
+        return null;
+    }
+
+    @Override
+    public void removeMedicalLogById(Long id) {
+
+    }
 }
