612489f7051d34ba0d42c55268c72e7205f4c2d7
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 18:54:06 +0200

    service-impl-updates

--- src/main/java/com/backend/inzira_care/service/VitalCheckService.java
+++ src/main/java/com/backend/inzira_care/service/VitalCheckService.java
@@ -1,2 +1,13 @@
-package com.backend.inzira_care.service;public interface VitalCheckService {
+package com.backend.inzira_care.service;
+
+import com.backend.inzira_care.model.VitalCheck;
+
+import java.util.List;
+
+public interface VitalCheckService {
+    List<VitalCheck> getAllVitalChecks();
+    VitalCheck getVitalCheckById(Long id);
+    VitalCheck saveVitalCheck(VitalCheck vitalCheck);
+    VitalCheck updateVitalCheck(Long id, VitalCheck vitalCheck);
+    void removeVitalCheckById(Long id);
 }
